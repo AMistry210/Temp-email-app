186225efe97bf5e6f465f83968f923f811967415
AMistry210 <116072137+AMistry210@users.noreply.github.com>
2025-01-14 22:10:59 +0000

    Updated tempemail class to save and load emails

--- src/main/java/io/github/amisty210/tempemailapp/TemporaryEmailApp.java
+++ src/main/java/io/github/amisty210/tempemailapp/TemporaryEmailApp.java
@@ -5,14 +5,38 @@ import java.util.List;
 
 public class TemporaryEmailApp {
     private List<Email> inbox;
+    private EmailStorage emailStorage;
 
     public TemporaryEmailApp() {
         inbox = new ArrayList<>();
+        emailStorage = new EmailStorage();
     }
 
     public void refreshApp() {
         inbox.clear();
         inbox.add(new Email());
+    }
+
+    public void loadEmails() {
+        try {
+            inbox = emailStorage.loadEmails();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveEmails() {
+        try {
+            emailStorage.saveEmails(inbox);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendEmail(String subject, String body) {
+        Email newEmail = new Email();
+        inbox.add(newEmail);
+        saveEmails();
     }
 
     public List<Email> getInbox() {
